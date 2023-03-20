1f3988639e35eaf1a43b6aaaaba5000571b9c982
ah-hatahet <48881982+ah-hatahet@users.noreply.github.com>
2023-03-20 00:25:26 -0400

    inital commit

---
+++ mlapi.py
@@ -0,0 +1,26 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+
+
+app = FastAPI()
+
+class ScoringItem(BaseModel):
+    SepalLength: float
+    SepalWidth: float
+    PetalLength: float
+    PetalWidth: float
+
+with open('model.pkl','rb') as f:
+    model = pickle.load(f)
+    
+
+@app.post('/')
+
+
+async def scoring_ençdpoint(item: ScoringItem):
+    df = pd.DataFrame([item.dict().values()],columns = item.dict().keys())
+    yhat = model.predict(df)
+
+    return {'prediction':float(yhat)}
